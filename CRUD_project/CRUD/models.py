7b27410e2e315d0f1bc10961f50d50748f09a130
PSHariNarayanan <harikrish.1093@gmail.com>
2020-02-09 23:05:01 +0530

    login and signup handling

--- CRUD_project/CRUD/models.py
+++ CRUD_project/CRUD/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model) :
+    username = models.CharField(max_length=225)
+    email = models.EmailField(max_length=225)
+    password = models.CharField(max_length=225)
--- CRUD_project/CRUD/views.py
+++ CRUD_project/CRUD/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import User
+import re
 
-# login view 
+# view for basic login
 def login(request) : 
     return render(request,'login.html')
 
+# view for handling login
 def loginHandler(request) :
+    username = request.POST.get('username')
+    password_form = request.POST.get('password')
+    if not username and not password_form :
+        return redirect('login')
+    else : 
+        user_details = User.objects.get(username=username)
+        if not user_details :
+            return redirect('login')
+        password_db = user_details.password
+        print(password_db)
+        if password_db==password_form :
+            return redirect('projects')
+        else :
+            return redirect('login')
 
-
-
-    return render(request,'projects.html')
-
+# view for basic signup
 def signUp(request) :
     return render(request,'signup.html')
 
+# view for handling signup
 def signupHandler(request) :
-
-
-    return render(request,'projects.html')
+    email = request.POST.get('email')
+    regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+    if not re.search(regex,email) :
+        return redirect('signup')
+    username = request.POST.get('username')
+    password1 = request.POST.get('password1')
+    password2 = request.POST.get('password2')
+    if password1!=password2 :
+         return redirect('signup')
+    user = User(username=username,email=email,password=password1)
+    user.save()
+    return redirect('projects')
